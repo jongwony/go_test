c0bead0d4addddd3f35be2b353e5333d1eb0d7de
jongwony <lastone9182@gmail.com>
2022-03-07 21:04:06 +0900

    go pkg import

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jongwony/go_test
+
+go 1.17
+
+require github.com/jongwony/go_polish v0.0.0-20220307115848-c29976e1841e
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jongwony/go_polish/pkg/tempconv"
+)
+
+func main() {
+	fmt.Println(tempconv.CToF(42))
+}
